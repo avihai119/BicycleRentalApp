ae17b984bc97997c0497c731a14430c154a6ef98
Avihai_Levi <118832985+avihai119@users.noreply.github.com>
2025-07-16 21:21:44 +0300

    Delete BicycleRental directory

--- BicycleRental/src/com/hit/dao/IDao.java
+++
@@ -1,7 +0,0 @@
-package com.hit.dao;
-
-public interface IDao<Long, T> {
-    void save(T entity);
-    void delete(T entity);
-    T search(long id);
-}
--- BicycleRental/src/com/hit/dm/Color.java
+++
@@ -1,5 +0,0 @@
-package com.hit.dm;
-
-public enum Color {
-    RED, BLUE, BLACK, WHITE
-}
